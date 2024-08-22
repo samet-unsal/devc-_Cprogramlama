5ff3538c8df40a89264b3769617eb26b3c78a0f9
samet-unsal <samettunsal@gmail.com>
2024-08-22 14:44:52 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Karar Yapýlarý Örnek */
+
+int main() 
+
+{
+	int sayi1,sayi2;
+	printf("Lutfen ilk sayiyi giriniz:");
+	scanf("%d",&sayi1);
+	printf("Lutfen ikinci sayiyi giriniz:");
+	scanf("%d",&sayi2);
+	
+	
+	if (sayi1 > sayi2)
+	{
+		printf("Ilk sayi buyuktur.\n");
+	}
+	
+	else if (sayi2 > sayi1)
+	{
+		printf("Ikinci sayi buyuktur.");
+	}
+	
+	else
+	{
+		printf("Sayilar birbirine esittir.");
+	}
+	
+	return 0;
+}
