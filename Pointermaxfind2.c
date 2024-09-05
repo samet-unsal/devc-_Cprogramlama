573e1d84228901d31fc8ca8c4e7ca2438af7e2c5
samet-unsal <samettunsal@gmail.com>
2024-09-05 10:31:22 +0300

    Update Pointermaxfind2.c

--- Pointermaxfind2.c
+++ Pointermaxfind2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Adresleri kullanarak indis bulma  */
+/* Pointerın adresini bulma  */
 int maxfind(int *a,int uzunluk);
 int main()
 {
@@ -9,9 +9,10 @@ int main()
 	int *enfazla,i;
 	enfazla = maxfind(sayilar,5);
 	printf("Sayilar dizisinin en buyuk elemani:%d\n",*enfazla);
-	printf("En buyuk eleman dizinin %d.elemanidir.",(enfazla-&sayilar[0]));//pointer adreslerini kullanarak dizinin kaçıncı eleman olduğunu bulduk.
+	printf("En buyuk eleman dizinin %d.elemanidir\n",(enfazla-&sayilar[0]));//pointer adreslerini kullanarak dizinin kaçıncı eleman olduğunu bulduk.
 	// istediğimiz elemanın adresinden dizinin ilk elemanının adresinden çıkararak kaçıncı eleman olduğunu buluruz : p(elemanımız)-&a[0](herhangi bir dizinin ilk elemanı).
 	//dizilerle çalışırken zaten aynı tür değişken olur ama yine de aynı tür değişken olmasına dikkat etmemiz gerekir.
+	printf("Pointerin adresi:%p",&enfazla);//pointerı da tutan bir adres vardır ve bu kodla o adresi yazdırdık.
 	
 	return 0;
 }
@@ -29,4 +30,3 @@ int maxfind(int *a,int uzunluk)
 	}
 	return pmax;
 }
-
